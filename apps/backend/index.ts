f773b2d128efe6d6b5806cf6fc3eb683e58e8331
JATIN <Jatin04072005@gmail.com>
2025-11-08 02:07:21 +0530

    adding TurboRepo

---
+++ apps/backend/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./Routers/userRoutes";
+import adminRoutes from "./Routers/adminRoutes";
+import contestRoutes from "./Routers/contestRoutes";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/contest", contestRoutes);
+
+
+app.get("/", (req: express.Request, res: express.Response) => {
+    res.status(200).send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
